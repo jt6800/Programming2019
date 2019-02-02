1cd85093675bdd4250494ed55bdc4a8486405194
jt6800 <45170921+jt6800@users.noreply.github.com>
2019-02-02 17:40:31 -0500

    Created BegClasses

---
+++ BegClasses/BegClasses/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BegClasses
+{
+    public class Animal
+    {
+        public string species;
+        public string name;
+        public string color;
+        public int age;
+
+        public Animal(string species, string name, string color)
+        {
+            this.species = species;
+            this.name = name;
+            this.color = color;
+            age = 0;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Grrrrr");
+        }
+        public void SetAge(int newAge)
+        {
+            this.age = newAge;
+        }
+
+        public void printDescription()
+        {
+            Console.Write(name);
+            Console.Write(" is a ");
+            Console.Write(age);
+            Console.Write(" year old ");
+            Console.Write(species);
+        }
+
+
+    }
+}
