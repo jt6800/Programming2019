ce4b2b2b91bffa4879aff3379587ecb47a2f002d
jt6800 <45170921+jt6800@users.noreply.github.com>
2019-02-02 20:00:29 -0500

    Created TestProjBegClasses

---
+++ TestProjBegClasses/TestProjBegClasses/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjBegClasses
+{
+    class Animal
+        {
+            public string species;
+            public string name;
+            public string color;
+            public int age;
+
+            public Animal(string species, string name, string color)
+            {
+                this.species = species;
+                this.name = name;
+                this.color = color;
+                age = 0;
+            }
+
+            public void MakeSound()
+            {
+                Console.WriteLine("Grrrrr");
+                Console.ReadKey();
+            }
+            public void SetAge(int newAge)
+            {
+                this.age = newAge;
+            }
+
+            public void printDescription()
+            {
+                Console.Write(name);
+                Console.Write(" is a ");
+                Console.Write(age);
+                Console.Write(" year old ");
+                Console.Write(species);
+                Console.ReadKey();
+            }
+        }
+}
