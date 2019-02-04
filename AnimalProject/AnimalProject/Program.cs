e0a1a41d1538e8938c333ae2fd9fc76c8ebfcb78
jt6800 <45170921+jt6800@users.noreply.github.com>
2019-02-04 08:07:58 -0500

    Updated AnimalProject

---
+++ AnimalProject/AnimalProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void menu()
+        {
+
+        }
+        
+        static dynamic getInput(string inputType)
+        {
+            dynamic variable;
+            if (inputType == "string")
+            {
+                variable = Console.ReadLine();
+                return variable;
+            }
+            else if (inputType == "int")
+            {
+                variable = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+
+}
