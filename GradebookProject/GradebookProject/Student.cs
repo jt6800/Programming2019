126d38597b776554067abd45bea60d1271e1a84f
jt6800 <45170921+jt6800@users.noreply.github.com>
2019-02-19 10:12:22 -0500

    Updated Gradebook

---
+++ GradebookProject/GradebookProject/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradebookProject
+{
+    class Student
+    {
+        string firstName;
+        string lastName;
+        string username;
+        string phoneNumber;
+
+        bool AddStudent(string firstName, string lastName, string username, string phoneNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.username = username;
+            this.phoneNumber = phoneNumber;
+            return true;
+        }
+    }
+}
