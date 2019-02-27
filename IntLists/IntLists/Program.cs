74636fdc1c8e839a59de9d8b3719811cbd2c5ca3
jt6800 <45170921+jt6800@users.noreply.github.com>
2019-02-26 21:26:01 -0500

    Updated IntLists

--- IntLists/IntLists/Program.cs
+++ IntLists/IntLists/Program.cs
@@ -20,10 +20,30 @@ namespace IntLists
             List<string> listOfStrings = new List<string>();
             List<string> studentName = new List<string>();
             List<int> intList = new List<int>();
-            List<double> dblList = new List<double>;
-
-
-
+            List<double> doubleList = new List<double>();
+            intList.Add(12);
+            intList.Add(7);
+            intList.Add(5);
+            intList.Add(3);
+            int intElement = intList[0];
+            Console.WriteLine(intElement);
+            intElement = intList[1];
+            Console.WriteLine(intElement);
+            intElement = intList[2];
+            Console.WriteLine(intElement);
+            intElement = intList[3];
+            Console.WriteLine(intElement);
+            dblList.Add(12.1);
+            dblList.Add(5.2);
+            dblList.Add(7.3);
+            double doubleElement = dblList[0];
+            Console.WriteLine(doubleElement);
+            doubleElement = dblList[1];
+            Console.WriteLine(doubleElement);
+            doubleElement = dblList[2];
+            Console.WriteLine(doubleElement);
+            doubleElement = dblList[3];
+            Console.WriteLine(doubleElement);
 
         }
     }
