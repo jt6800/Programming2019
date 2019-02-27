e1b23d547dc7efa31572d8bd4bce86602875d791
jt6800 <45170921+jt6800@users.noreply.github.com>
2019-02-26 19:40:34 -0500

    Create IntLists

---
+++ IntLists/IntLists/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intgerList = new List<int>();
+            List<int> listOfIntegers = new List<int>();
+            List<int> testScores = new List<int>();
+            List<double> dblList = new List<double>();
+            List<double> listOfDoubles = new List<double>();
+            List<double> playerPoints = new List<double>();
+            List<string> stringList = new List<string>();
+            List<string> listOfStrings = new List<string>();
+            List<string> studentName = new List<string>();
+            List<int> intList = new List<int>();
+            List<double> dblList = new List<double>;
+
+
+
+
+        }
+    }
+}
