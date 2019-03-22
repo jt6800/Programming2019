f584f1c6e9fb21215fb87ecb4a66ed5a15850795
jt6800 <45170921+jt6800@users.noreply.github.com>
2019-03-22 10:18:48 -0400

    Recreated Gradebook

---
+++ Gradebook/Gradebook/Gradebook.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook
+{
+    class Gradebook
+    {
+     //properties
+     //list/array of sections
+     //SectionName - string
+
+
+        //methods
+
+        //sets the currently active section - needs to set the currently active section
+        //to the section with the given sectionName.
+        // Returns: true if it succeeds false if it fails(no section that matches that name).
+        public bool changeSection(string sectionName)
+        {
+            return false; //FIXME
+        }
+
+        //creates a new section if there are not already 6 sections
+        // and sectionName is not already used , also need to set the currentSection to be the new section
+        // Returns: true if it succeeds false if it fails.
+        public bool addSection(string sectionName)
+        {
+            return false; //FIXME
+        }
+        //Creates a new student and adds the student to the currently active section
+        //returns: True if it succeeds, false if it fails(a student with the same username already exists or there are no sections) 
+        public bool addStudent(string firstName, string lastName, string username, long phoneNumber)
+        {
+            return false; //FIXME
+        }
+
+        //returns overall score (total points earned for all assignments/ total points possible) as a percentage for specified student
+        //returns -1 if the student was not found or if the student did not have any assignments assigned or there are no sections.
+        public double getOverallScore(string username)
+        {
+            return -1; //FIXME
+        }
+        //returns overall score as a percentage for the current section - (the average of each students overall score.)
+        //Each student should have the same weight even if they have different number of points possible
+        //IE: get each student's overall score as a percentage and calculate the average percentage for the students .
+        //do not count students with no assignments assigned in the average.
+        //returns -1 if there were no assignments in the section or there are no sections.
+        public double getOverallScoreAvg()
+        {
+            return -1; //FIXME
+        }
+
+
+        //creates a new assignment and adds it to the student specified by username.
+        //Returns true if it succeeds and false if the student is not found or the student already has an assignment with the
+        //same assignmentName or there are no sections
+        public bool addAssignmentToStudent(string username, string assignmentName, int pointsPossible)
+        {
+            return false; //FIXME
+        }
+
+        //checks to make sure no student already has an assignment with the same assignment name first
+        //if there is no existing assignment it creates a new assignment and adds the assignment to all
+        // the students in the current section.
+        //returns: True if successful and false if at least one student has the same assignment name or there are no sections
+        public bool addAssignmentToSection(string assignmentName, int pointsPossible)
+        {
+            return false;//FIXME
+        }
+
+        //sets the pointsEarned property for the student specified in the current section for the assignment specified
+        //returns: true if successful, false if the student is not found or the assignmentName is not found  or there are no sections.
+        public bool setScore(string username, string assignmentName, int pointsEarned)
+        {
+            return false;//FIXME
+        }
+
+        //returns score on assignment specified as a percentage for the specified student in the current section
+        //returns -1 if the student was not found or the student did not have an assignment with the specified assignment name or there are no sections
+        public double getAssignmentScorePercent(string username, string assignmentName)
+        {
+            return -1;//FIXME
+        }
+
+        //returns the average score as a percentage for the current section.
+        //only the student with the assignment assigned to them will be counted in the average.
+        //returns -1 if no student had an assignment with the specified assignment name  or there are no sections
+        public double getAssignmentScoreAvg(string assignmentName)
+        {
+            return -1;//FIXME
+        }
+
+        //adds 1 to the specified student's tardy count.
+        //returns: true if successful, false if the student was not found in the current section  or there are no sections
+        public bool markTardy(string username)
+        {
+            return false;//FIXME
+        }
+
+        //adds 1 to the specified student's absent count.
+        //returns: true if successful, false if the student was not found in the current section  or there are no sections
+        public bool markAbsent(string username)
+        {
+            return false; //FIXME
+        }
+
+        //returns the student tardy count for the specified student, it returns -1 if the student was not found  or there are no sections
+        public int getTardyCount(string username)
+        {
+            return -1; //FIXME
+        }
+
+        //returns the student tardy count for the specified student, it returns -1 if the student was not found  or there are no sections
+        public int getAbsentCount(string username)
+        {
+            return -1; //FIXME
+        }
+    }
+}
---
+++ Gradebook/Gradebook/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gradebook gradebook = new Gradebook();
+            //set up optionsArray
+            string[] options = new string[14];
+            options[0] = "change section";
+            options[1] = "add section";
+            options[2] = "add student";
+            options[3] = "add assignment to student";
+            options[4] = "add assignment to section";
+            options[5] = "set score";
+            options[6] = "mark tardy";
+            options[7] = "mark absent";
+            options[8] = "get overall score";
+            options[9] = "get overall score avg";
+            options[10] = "get assignment score";
+            options[11] = "get assignment score avg";
+            options[12] = "get tardy count";
+            options[13] = "get absent count";
+
+
+            //main loop to get input:
+            while (true)
+            {
+                //print options
+                Console.WriteLine("Type one of the following commands:");
+                for (int i = 0; i < options.Length; i++)
+                {
+                    Console.WriteLine(options[i]);
+                }
+                string input = Console.ReadLine();
+                //find out which option the selected
+                int optionSelectIndex;
+                for (optionSelectIndex = 0; optionSelectIndex < options.Length; optionSelectIndex++)
+                {
+                    if (options[optionSelectIndex].Equals(input))
+                    {
+                        break;
+                    }
+                }
+
+                bool retBool = false;
+                double retDouble = -1;
+                if (optionSelectIndex == 0)
+                {
+                    Console.WriteLine("Enter a section name");
+                    string sectionName = Console.ReadLine();
+                    retBool = gradebook.changeSection(sectionName);
+                }
+                else if (optionSelectIndex == 1)
+                {
+                    Console.WriteLine("Enter a section name");
+                    string sectionName = Console.ReadLine();
+                    retBool = gradebook.addSection(sectionName);
+                }
+                else if (optionSelectIndex == 2)
+                {
+                    Console.WriteLine("Enter student first name:");
+                    string firstName = Console.ReadLine();
+                    Console.WriteLine("Enter student last name:");
+                    string lastName = Console.ReadLine();
+                    Console.WriteLine("Enter student username:");
+                    string userName = Console.ReadLine();
+                    long phoneNumber = getPhoneNumber();
+                    retBool = gradebook.addStudent(firstName, lastName, userName, phoneNumber);
+                }
+                else if (optionSelectIndex == 3)
+                {
+                    Console.WriteLine("Enter student username:");
+                    string userName = Console.ReadLine();
+                    Console.WriteLine("Enter assignment name:");
+                    string assignmentName = Console.ReadLine();
+                    int ptsPossible = getUInt();
+                    retBool = gradebook.addAssignmentToStudent(userName, assignmentName, ptsPossible);
+                }
+                else if (optionSelectIndex == 4)
+                {
+                    Console.WriteLine("Enter assignment name:");
+                    string assignmentName = Console.ReadLine();
+                    int ptsPossible = getUInt();
+                    retBool = gradebook.addAssignmentToSection(assignmentName, ptsPossible);
+                }
+                else if (optionSelectIndex == 5)
+                {
+                    Console.WriteLine("Enter student username:");
+                    string userName = Console.ReadLine();
+                    Console.WriteLine("Enter assignment name:");
+                    string assignmentName = Console.ReadLine();
+                    int ptsEarned = getUInt();
+                    retBool = gradebook.setScore(userName, assignmentName, ptsEarned);
+                }
+                else if (optionSelectIndex == 6)
+                {
+                    Console.WriteLine("Enter student username:");
+                    string userName = Console.ReadLine();
+                    retBool = gradebook.markTardy(userName);
+                }
+                else if (optionSelectIndex == 7)
+                {
+                    Console.WriteLine("Enter student username:");
+                    string userName = Console.ReadLine();
+                    retBool = gradebook.markAbsent(userName);
+                }
+                else if (optionSelectIndex == 8)
+                {
+                    Console.WriteLine("Enter student username:");
+                    string userName = Console.ReadLine();
+                    retDouble = gradebook.getOverallScore(userName);
+                }
+                else if (optionSelectIndex == 9)
+                {
+                    retDouble = gradebook.getOverallScoreAvg();
+                }
+                else if (optionSelectIndex == 10)
+                {
+                    Console.WriteLine("Enter student username:");
+                    string userName = Console.ReadLine();
+                    Console.WriteLine("Enter assignment name:");
+                    string assignmentName = Console.ReadLine();
+                    retDouble = gradebook.getAssignmentScorePercent(userName, assignmentName);
+                }
+                else if (optionSelectIndex == 11)
+                {
+                    Console.WriteLine("Enter assignment name:");
+                    string assignmentName = Console.ReadLine();
+                    retDouble = gradebook.getAssignmentScoreAvg(assignmentName);
+                }
+                else if (optionSelectIndex == 12)
+                {
+                    Console.WriteLine("Enter student username:");
+                    string userName = Console.ReadLine();
+                    retDouble = gradebook.getTardyCount(userName);
+                }
+                else if (optionSelectIndex == 13)
+                {
+                    Console.WriteLine("Enter student username:");
+                    string userName = Console.ReadLine();
+                    retDouble = gradebook.getAbsentCount(userName);
+                }
+                else
+                {
+                    Console.WriteLine("not a valid option");
+                    continue;
+                }
+                if (retBool)
+                {
+                    Console.WriteLine("action completed successfully");
+                }
+                else if (retDouble != -1)
+                {
+                    Console.WriteLine("Result from command: " + retDouble);
+                }
+                else
+                {
+                    Console.WriteLine("There was an error with the command, operation not completed");
+                }
+            }
+
+        }
+        static long getPhoneNumber()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("enter a phone number including area code as a 10 digit number");
+                    long phoneNumber = Convert.ToInt64(Console.ReadLine());
+                    if (phoneNumber < 1000000000l || phoneNumber > 9999999999l)
+                    {
+                        Console.WriteLine("not a valid phone number enter a number including area code without space or - etc");
+                        continue;
+                    }
+                    return phoneNumber;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("not an integer");
+                }
+            }
+        }
+        static int getUInt()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter amount of points");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    if (number < 0)
+                    {
+                        Console.WriteLine("not a positive integer");
+                        continue;
+                    }
+                    return number;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("not an integer");
+                }
+            }
+        }
+    }
+}
+    }
+}
