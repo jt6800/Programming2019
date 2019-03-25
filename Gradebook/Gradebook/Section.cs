f1555022dcbf655913110a55a82897ce4b0dec9f
jt6800 <45170921+jt6800@users.noreply.github.com>
2019-03-25 08:14:38 -0400

    Updated Gradebook
    Lots of problems with references, not quite sure what's going on, unable to refer to objects/"items" created in immediatly preceeding lines (best example line 12/13 of Section.cs)

---
+++ Gradebook/Gradebook/Section.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook
+{
+    class Section
+    {
+        string sectionName;
+        List<Students> studentList = new List<Students>();
+        int studentLength = studentList.length();
+
+    }
+}
---
+++ Gradebook/Gradebook/Students.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook
+{
+    class Students
+    {
+        string FirstName = null;
+        string LastName = null;
+        long studentID = 0;
+        long phoneNUmber = 0;
+        unsafe
+	    {
+
+	
+            static Students StudentConstructor()
+
+            {
+            Students student1 = new Students();
+            return student1;
+        
+            }
+        }
+    }
+}
